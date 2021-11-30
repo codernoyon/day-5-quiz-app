eef19047049d3a193708de1df843d96a2893d316
codernoyon <noyonrahman2003@gmail.com>
2021-11-30 17:48:47 +0600

    new change added

--- js/main.js
+++ js/main.js
@@ -20,4 +20,19 @@ continueBtn.onclick = () => {
     questionBox.classList.add('questions_box_active');
     rulesBox.classList.remove('active_rules_box');
     myQuizApp.classList.add('inactive_my_quiz_app');
+    showQuestions(1);
+}
+
+
+function showQuestions(index) {
+    const mainQuestion = document.querySelector('.main_question');
+    let  queTag = "<span>" + questions[index].numb + "." + questions[index].question + "</span>";
+    mainQuestion.innerHTML = queTag; //main question here
+    const optionList = document.querySelector('.my_options'); //option list here
+    let optionTag = '<div class="option">' + questions[index].options[0] + '</div>'
+    +'<div class="option">' + questions[index].options[1] + '</div>'
+    +'<div class="option">' + questions[index].options[2] + '</div>'
+    +'<div class="option">' + questions[index].options[3] + '</div>'
+    
+    optionList.innerHTML = optionTag;
 }
